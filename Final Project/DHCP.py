e9b08337a1224debb21e56bf41519fa7dc40d723
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-03-08 21:13:26 +0200

    Update DHCP.py

---
+++ Final Project/DHCP.py
@@ -0,0 +1,57 @@
+from scapy.all import*
+import time
+from scapy.layers.dhcp import DHCP, BOOTP
+from scapy.layers.inet import IP, UDP
+from scapy.layers.l2 import Ether
+
+global ip_dhcp, ip_end, ip_list
+ip_dhcp = "10.0.0.16"
+ip_end = 16
+ip_list = ["10.0.0.16", "10.0.0.18", "10.0.0.10"]
+
+def offer(packet):
+    global ip_end, ip_list
+    print("Packet received!")
+    if DHCP in packet and packet[DHCP].options[0][1] == 1: #Discover check
+        client_ip = "10.0.0." + str(ip_end)
+        while client_ip in ip_list: #Generate IP
+            print("IP: ", client_ip, "is already in use")
+            ip_end += 1
+            if ip_end > 254:
+                print("Reached IP limit!")
+                client_ip = "0.0.0.0" #IP limit reached so i assign 0.0.0.0 as IP
+                break
+            client_ip = "10.0.0." + str(ip_end)
+        offer = Ether(dst="ff:ff:ff:ff:ff:ff") / \
+                 IP(src=ip_dhcp, dst="255.255.255.255") / \
+                 UDP(sport=67, dport=68) / \
+                 BOOTP(op=2, yiaddr=client_ip, siaddr=ip_dhcp, giaddr="0.0.0.0", xid=packet[BOOTP].xid) / \
+                 DHCP(options=[("message-type", "offer"),
+                    ("subnet_mask", "255.255.255.0"),
+                    ("router", "10.0.0.10"),
+                    ("name_server", "10.0.0.18"),
+                    ("lease_time", 3600), "end"])
+        time.sleep(1)
+        sendp(offer, iface="wlp3s0")
+        print("Offer sent!")
+        sniff(filter="udp and port 68", prn=ack, count=1, iface="wlp3s0")
+
+def ack(packet):
+    global ip_end, ip_list
+    if DHCP in packet and packet[DHCP].options[0][1] == 3:  # Request Check
+        ip_list.append(packet[BOOTP].yiaddr)
+        print("List of IP's: ")
+        print(ip_list)
+        dhcp_ack = Ether(dst="ff:ff:ff:ff:ff:ff") / \
+                   IP(src=ip_dhcp, dst="255.255.255.255") / \
+                   UDP(sport=67, dport=68) / \
+                   BOOTP(op=2, yiaddr=packet[BOOTP].yiaddr, siaddr=packet[BOOTP].siaddr, giaddr="0.0.0.0", xid=packet[BOOTP].xid) / \
+                   DHCP(options=[("message-type", "ack"),
+                                 ("subnet_mask", "255.255.255.0"),
+                                 ("router", "10.0.0.18"),
+                                 ("lease_time", 3600), "end"]) #ACK packet created
+        time.sleep(1)
+        sendp(dhcp_ack, iface="wlp3s0") #Send ACK
+        print("ACK sent!")
+if __name__ == '__main__':
+    sniff(filter="udp and port 68", prn=offer, iface="wlp3s0")
