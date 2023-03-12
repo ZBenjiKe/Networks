748dd31e4d8dc3a097761637c7060b91018b45d1
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-03-12 17:22:52 +0200

    Update Client.py

--- Final Project/Client.py
+++ Final Project/Client.py
@@ -1,21 +1,23 @@
 from socket import *
-from scapy.all import*
+from scapy.all import *
 import time
 from scapy.layers.dhcp import DHCP, BOOTP
 from scapy.layers.dns import DNSQR, DNS
 from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
 
+sequence_list = []
 '''''''''''''''''''''''''''''''''
     Connect with UDP to DHCP
 '''''''''''''''''''''''''''''''''
 
+
 def discover():
     first = Ether(dst="ff:ff:ff:ff:ff:ff") / \
-                    IP(src='0.0.0.0', dst='255.255.255.255') / \
-                    UDP(sport=68, dport=67) / \
-                    BOOTP(chaddr="74:e5:f9:0c:ea:ef", xid=0x12345678) / \
-                    DHCP(options=[("message-type", "discover"), "end"]) #Had a problem with xid generate i left it that way
+            IP(src='0.0.0.0', dst='255.255.255.255') / \
+            UDP(sport=68, dport=67) / \
+            BOOTP(chaddr="74:e5:f9:0c:ea:ef", xid=0x12345678) / \
+            DHCP(options=[("message-type", "discover"), "end"])  # Had a problem with xid generate i left it that way
     sendp(first)
     print("Discover sent!")
     sniff(filter="udp and port 67", prn=request, count=1)
@@ -29,22 +31,23 @@ def request(packet):
     if client_ip == "0.0.0.0":
         print("IP address didn't assigned")
         return
-    print ("Client IP:", client_ip)
+    print("Client IP:", client_ip)
     print("DNS:", dns_ip)
     request = Ether(dst="ff:ff:ff:ff:ff:ff") / \
-                    IP(src="0.0.0.0", dst="255.255.255.255") / \
-                    UDP(sport=68, dport=67) / \
-                    BOOTP(chaddr="74:e5:f9:0c:ea:ef", yiaddr=client_ip, xid=packet[BOOTP].xid) / \
-                    DHCP(options=[("message-type", "request"), "end"])
+              IP(src="0.0.0.0", dst="255.255.255.255") / \
+              UDP(sport=68, dport=67) / \
+              BOOTP(chaddr="74:e5:f9:0c:ea:ef", yiaddr=client_ip, xid=packet[BOOTP].xid) / \
+              DHCP(options=[("message-type", "request"), "end"])
     time.sleep(1)
     sendp(request)
     print("Request sent!")
-    #sniff(filter="udp and port 67", count=1)
+    # sniff(filter="udp and port 67", count=1)
 
 
 '''''''''''''''''''''''''''''''''
     Connect with UDP to DNS
 '''''''''''''''''''''''''''''''''
+
 
 def getDashIP(dns_ip, dns_port, app_domain):
     clientSocket = socket.socket(AF_INET, SOCK_DGRAM)
@@ -73,11 +76,11 @@ def getDashIP(dns_ip, dns_port, app_domain):
           TCP - DASH
 '''''''''''''''''''''''''''''''''
 
+
 def streamFromDashTCP(app_ip, app_port):
     DASH_ADDRESS = (app_ip, app_port)
     clientSocket = socket.socket(AF_INET, SOCK_STREAM)
     clientSocket.connect(DASH_ADDRESS)
-
     '''
     Make sure process port is 20908
     '''
@@ -88,16 +91,26 @@ def streamFromDashTCP(app_ip, app_port):
     while chosenQuality != "720" and chosenQuality != "480" and chosenQuality != "360":
         chosenQuality = input('Please enter a valid choice')
     clientSocket.send(chosenQuality.encode())
-    
+
     # Receive video files
     time.sleep(1)
 
     frameCount = 1
 
-    while(frameCount <= 25):
+    while (frameCount <= 25):
         data = b''
         while b"Finished" not in data:
             data += clientSocket.recv(1024)
+            packet_str = data.decode("utf-8")  # Convert bytes to string
+            sequence_number = int(packet_str.split(",")[1].split(" ")[-1])
+            sequence_list.append(sequence_number)
+            if (len(sequence_list)<sequence_number):
+                resend = extractUndelivered(sequence_list)
+                # Construct the request message
+                request = f"Send:{resend}:please".encode()
+                # Send the data message
+                clientSocket.sendto(request, DASH_ADDRESS)
+                data += clientSocket.recv(1024)
         data = data[:-8]
 
         image_copy = open(f'Copies/{frameCount}.png', "wb")
@@ -110,6 +123,13 @@ def streamFromDashTCP(app_ip, app_port):
 
     clientSocket.close()
 
+def extractUndelivered(sequence_list):
+    for i, num in enumerate(sequence_list):
+        if i == 0:
+            continue
+        if num != sequence_list[i-1]+1:
+            return num-1
+    return None  # If no number is missing, return None
 
 def streamFromDashRUDP(app_ip, app_port):
     DASH_ADDRESS = (app_ip, app_port)
@@ -149,18 +169,18 @@ def streamFromDashRUDP(app_ip, app_port):
 def main():
     dhcp_ip = '127.0.0.1'
     dhcp_port = 67
-    #dns_ip = '0.0.0.0'
+    # dns_ip = '0.0.0.0'
     dns_port = 53
     app_ip = '0.0.0.0'
     app_port = 30577
     app_domain = "www.dashserver.com"
 
-    #dns_ip = discover()
-    #if dns_ip != "0.0.0.0":
+    # dns_ip = discover()
+    # if dns_ip != "0.0.0.0":
     #    app_ip = getDashIP(dns_ip, dns_port, app_domain)
-    #if app_ip != "0.0.0.0":
-        #streamFromDashTCP(app_ip, app_port)
-        #streamFromDashRUDP(app_ip, app_port)
+    # if app_ip != "0.0.0.0":
+    # streamFromDashTCP(app_ip, app_port)
+    # streamFromDashRUDP(app_ip, app_port)
     streamFromDashRUDP('127.0.0.1', app_port)
 
 
