463dda69bf767fc7ee6581d6d56d3849da6a286e
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2023-03-12 17:23:25 +0200

    Update DASH.py

--- Final Project/DASH.py
+++ Final Project/DASH.py
@@ -3,7 +3,6 @@ from socket import *
 import os
 import time
 
-
 img_urls_files = [
     '''
     fill in urls - from files
@@ -29,6 +28,7 @@ def downloadImage(img_url):
 
 '''
 
+
 def sendImagesOverTCP():
     SERVER_ADDRESS = ('', 30577)
     # serverSocket = socket.socket()
@@ -39,18 +39,18 @@ def sendImagesOverTCP():
     print("The DASH server is ready for connections.")
 
     clientSocket, addrClient = serverSocket.accept()
-    print("Recieved connection from client ",addrClient)
+    print("Recieved connection from client ", addrClient)
 
     message = "Hello from DASH!\nThere are 25 video frames available for this video.\nPlease select the video quality you would like:\n720, 480 or 360\n"
     clientSocket.send(bytes(message.encode()))
     quality = clientSocket.recv(1024).decode()
 
-    img_dir = os.getcwd()+"/Pictures"
+    img_dir = os.getcwd() + "/Pictures"
     frameCount = 1
 
-    while(frameCount <= 25):
+    while (frameCount <= 25):
         start = time.perf_counter()
-        imageFile = img_dir+f'/{quality}'+f"/{frameCount}.png"
+        imageFile = img_dir + f'/{quality}' + f"/{frameCount}.png"
         with open(imageFile, "rb") as imageFile:
             imageStr = imageFile.read()
         clientSocket.sendall(imageStr)
@@ -61,20 +61,20 @@ def sendImagesOverTCP():
         new_quality = verifyTCPQuality(start, finish, quality)
         quality = new_quality
     print("Finished sending frames to client.")
-    
+
     time.sleep(3)
     serverSocket.close()
 
 
 def verifyTCPQuality(start, finish, quality):
-    if (finish - start) > 1 :
+    if (finish - start) > 1:
         if quality == "720":
             quality = "480"
             print("quality changed from 720 to 480")
         elif quality == "480":
             quality = "360"
             print("quality changed from 480 to 360")
-    elif (finish - start) < 0.5 :
+    elif (finish - start) < 0.5:
         if quality == "360":
             quality = "480"
             print("quality changed from 360 to 480")
@@ -83,37 +83,37 @@ def verifyTCPQuality(start, finish, quality):
             print("quality changed from 480 to 720")
     return quality
 
-
-def receive_syn_message():
-    # Receive SYN message from the client
-    syn_message, address = serverSocket.recvfrom(BUFFER_SIZE)
-    print('Received SYN message from client:', syn_message.decode())
-    return syn_message, address
-
-
-def send_syn_ack_message(syn_message, address):
-    # Send SYN-ACK message to the client
-    global sequence_number
-    syn_ack_message = 'SYN-ACK:{}:{}'.format(syn_message.decode(), sequence_number)
-    serverSocket.sendto(syn_ack_message.encode(), address)
-    print('Sent SYN-ACK message to client:', syn_ack_message)
-
-
-def receive_ack_message(clientAddress):
-    # Receive ACK message from the client
-    global expected_sequence_number
-    ack_message, address = serverSocket.recvfrom(BUFFER_SIZE)
-    if ack_message.decode().startswith('ACK:') & clientAddress == address:
-        ack_sequence_number = int(ack_message.decode().split(':')[1])
-        if ack_sequence_number == expected_sequence_number:
-            print('Received ACK message from client:', ack_message.decode())
-            expected_sequence_number += 1
-            return True
-    return False
-
+#
+# def receive_syn_message():
+#     # Receive SYN message from the client
+#     syn_message, address = serverSocket.recvfrom(BUFFER_SIZE)
+#     print('Received SYN message from client:', syn_message.decode())
+#     return syn_message, address
+#
+#
+# def send_syn_ack_message(syn_message, address):
+#     # Send SYN-ACK message to the client
+#     global sequence_number
+#     syn_ack_message = 'SYN-ACK:{}:{}'.format(syn_message.decode(), sequence_number)
+#     serverSocket.sendto(syn_ack_message.encode(), address)
+#     print('Sent SYN-ACK message to client:', syn_ack_message)
+#
+#
+# def receive_ack_message(clientAddress):
+#     # Receive ACK message from the client
+#     global expected_sequence_number
+#     ack_message, address = serverSocket.recvfrom(BUFFER_SIZE)
+#     if ack_message.decode().startswith('ACK:') & clientAddress == address:
+#         ack_sequence_number = int(ack_message.decode().split(':')[1])
+#         if ack_sequence_number == expected_sequence_number:
+#             print('Received ACK message from client:', ack_message.decode())
+#             expected_sequence_number += 1
+#             return True
+#     return False
+#
 
 def sendImagesOverRUDP():
-    SERVER_ADDRESS = ('',30577)
+    SERVER_ADDRESS = ('', 30577)
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serverSocket.bind(SERVER_ADDRESS)
@@ -148,7 +148,8 @@ def sendImagesOverRUDP():
     serverSocket.close()
 
 
-def sendImage(imageFile, frameCount, client_address, serverSocket)
+def sendImage(imageFile, frameCount, client_address, serverSocket):
+    global resend_sequence
     with open(imageFile, "rb") as img:
         while True:
             image_data = img.read(1000)
@@ -163,44 +164,43 @@ def sendImage(imageFile, frameCount, client_address, serverSocket)
 
             # Construct the data message
             segment = f"Data:{frameCount}:{sequence_number}:{len(image_data)}".encode() + image_data
-            
+
             # Send the data message
             serverSocket.sendto(segment, client_address)
             print(f"Sent data message to client: Frame {frameCount}, Seq {sequence_number}")
-
+            # Define a buffer size for receiving data
+            BUFFER_SIZE = 1024
             # Wait for acknowledgement
             ack_received = False
-            while not ack_received:
-            try:
-                serverSocket.settimeout(timeout)
-                ack, _ = serverSocket.recvfrom(BUFFER_SIZE)
-                if ack.decode() == f"Ack:{sequence_number}":
-                ack_received = True
-                expected_sequence_number += 1
-                sequence_number += 1
+            nack_received = False
+            while not ack_received or not nack_received:
+                try:
+                    serverSocket.settimeout(timeout)
+                    ack, _ = serverSocket.recvfrom(BUFFER_SIZE)
+                    if ack.decode() == f"Ack:{sequence_number}":
+                        ack_received = True
+                    if int(ack.decode("utf-8").split(":")[0]) == "Send":
+                        nack_received = True
+                        resend_sequence = int(ack.decode("utf-8").split(":")[1])
+                    expected_sequence_number += 1
+                    sequence_number += 1
 
-            except socket.timeout:
-                print(f"Timeout occurred while waiting for ACK of sequence number {sequence_number}")
-                congestion_window = max(1, congestion_window // 2)  # divide congestion window by 2 on timeout
-                sequence_number = expected_sequence_number  # reset sequence number to last successfully transmitted frame
+                except socket.timeout:
+                    print(f"Timeout occurred while waiting for ACK of sequence number {sequence_number}")
+                    congestion_window = max(1, congestion_window // 2)  # divide congestion window by 2 on timeout
+                    sequence_number = expected_sequence_number  # reset sequence number to last successfully transmitted frame
 
-            # Define the congestion window and threshold size
-            congestion_window = 1
-            threshold = 16
-
-            # Define the flow control window size
-            flow_control_window = 1024
-
+            if nack_received == True:
+                resend(resend_sequence)
             sequence_number = 1
             expected_sequence_number = 1
             congestion_window = 1
             flow_control_window = 1024  # maximum amount of data to send in one go
             threshold = 16
-            timeout = 5 # in seconds
+            timeout = 5  # in seconds
 
             # Send termination message
         serverSocket.sendto(b"Finished", client_address)
-
 
         # Perform congestion control
         if congestion_window < flow_control_window:
@@ -213,14 +213,14 @@ def sendImage(imageFile, frameCount, client_address, serverSocket)
 
 
 def verifyUDPQuality(start, finish, quality):
-    if (finish - start) > 1 :
+    if (finish - start) > 1:
         if quality == "720":
             quality = "480"
             print("quality changed from 720 to 480")
         elif quality == "480":
             quality = "360"
             print("quality changed from 480 to 360")
-    elif (finish - start) < 0.5 :
+    elif (finish - start) < 0.5:
         if quality == "360":
             quality = "480"
             print("quality changed from 360 to 480")
@@ -231,8 +231,9 @@ def verifyUDPQuality(start, finish, quality):
 
 
 def main():
-    #sendImagesOverTCP()
+    # sendImagesOverTCP()
     sendImagesOverRUDP()
+
 
 if __name__ == '__main__':
     main()
